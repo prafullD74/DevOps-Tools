db6551c2fb80c8f4b838dee312ca24b1ba03af5d
Praful <prafull74@live.com>
2026-01-23 16:19:24 +0530

    Update main.tf
    variables

--- Terraform/AWS/Terraform-AWS-tutorial-EC2/main.tf
+++ Terraform/AWS/Terraform-AWS-tutorial-EC2/main.tf
@@ -14,10 +14,12 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
+   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  instance_type = var.instance_type
 
   tags = {
-    Name = "learn-terraform"
+   Name = "learn-terraform"
+   Name = var.instance_name
   }
 }
