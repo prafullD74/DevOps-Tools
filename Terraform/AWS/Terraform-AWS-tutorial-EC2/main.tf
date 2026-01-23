ec47df6d793b0b9bd8cb8ef15881d2e7c8dc710e
Praful <prafull74@live.com>
2026-01-23 16:35:14 +0530

    Update main.tf
    VPC module

--- Terraform/AWS/Terraform-AWS-tutorial-EC2/main.tf
+++ Terraform/AWS/Terraform-AWS-tutorial-EC2/main.tf
@@ -2,6 +2,20 @@ provider "aws" {
   region = "us-west-2"
 }
 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name = "example-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_dns_hostnames    = true
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -17,6 +31,9 @@ resource "aws_instance" "app_server" {
    ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   instance_type = var.instance_type
+
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.private_subnets[0]
 
   tags = {
    Name = "learn-terraform"
