13a4603b4e57404c4c3ef4507c900072c1396466
Praful <prafull74@live.com>
2026-01-30 15:54:55 +0530

    Update main.tf

--- Terraform/Azure/Terraform-Azure-tutorial/main.tf
+++ Terraform/Azure/Terraform-Azure-tutorial/main.tf
@@ -1,3 +1,24 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
+
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
   name                = "myTFVnet"
